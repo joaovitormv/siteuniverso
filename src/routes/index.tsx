fc2ecba9d4fc1ec9ad32c0ba0401a43e04a0def9
joaovitormv <joaovitorjjabuti@gmail.com>
2023-11-17 10:38:43 -0300

    por hoje é só

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,24 @@
 import { Routes, Route } from "react-router-dom"
-import { PageHome, PageList, PageLayout } from "../pages"
-
-export function RouteSoftware() {
+import { AdmPage, RegisterPage, LoginPage, PageHome, PageList, PageLayout } from "../pages"
+import ProtectedRoute from "./ProtectedRoute"
+const Rotas = () => {
   return (
     <Routes>
       <Route path="/" element={<PageLayout />}>
         <Route path="" element={<PageHome />} />
         <Route path="list" element={<PageList />} />
+        <Route path="/cadastrar" element={<RegisterPage/>}/>
+        <Route path="/logar" element={<LoginPage/>}/>
+        <Route element={<ProtectedRoute/>}>
+          <Route path="adm" element={<LoginPage/>}/>
+        </Route>
       </Route>
     </Routes>
   )
 }
+
+export function RouteSoftware() {
+
+}
+
+export default Rotas
